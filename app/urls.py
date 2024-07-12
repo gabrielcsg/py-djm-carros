a62500accfdd0b0f52212f10617d4639b6f23787
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-12 19:10:54 -0300

    feat: base and cars templates

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,5 @@ from cars import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', views.cars_view),
+    path('cars/', views.cars_view, name='cars_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
