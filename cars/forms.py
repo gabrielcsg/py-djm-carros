ca3dd17e0222bb8c1caebd5041c9835cb0612aac
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-13 16:02:00 -0300

    feat: new car form validations

--- cars/forms.py
+++ cars/forms.py
@@ -1,3 +1,4 @@
+from typing import Self
 from django import forms
 from cars.models import Car
 
@@ -6,3 +7,20 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self: Self):
+        value = self.cleaned_data.get('value')
+        if value and value < 20000:
+            self.add_error(
+                'value', 'Valor mínimo do carro deve ser de R$20.000'
+            )
+
+        return value
+
+    def clean_factory_year(self: Self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year and factory_year < 1975:
+            self.add_error(
+                'factory_year', 'Não é possível cadastrar carros fabricados antes de 1975')
+
+        return factory_year
