9c0b3ad54f49b66dfa3f66d39117621cb0b99709
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-14 17:38:43 -0300

    feat: signals and car inventory

--- cars/models.py
+++ cars/models.py
@@ -22,3 +22,15 @@ class Car(models.Model):
 
     def __str__(self) -> str:
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']  # descending order (-)
+
+    def __str__(self) -> str:
+        return f'{self.cars_count} - {self.cars_value}'
