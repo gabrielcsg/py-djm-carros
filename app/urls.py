ca9dc83aa21b42219b6a544bf01340ebf6c14722
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-14 02:00:34 -0300

    feat: change cars views

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from accounts.views import login_view, logout_view, register_view
-from cars import views
+from cars.views import CarsListView, NewCarCreateView
 
 
 urlpatterns = [
@@ -12,6 +12,6 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('cars/', views.cars_view, name='cars_list'),
-    path('cars/new/', views.new_car_view, name='new_car'),
+    path('cars/', CarsListView.as_view(), name='cars_list'),
+    path('cars/new/', NewCarCreateView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
