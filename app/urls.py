e3a3de35867f61c7aa902d67fb8714729476b596
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-14 15:47:59 -0300

    feat: cars update and delete

--- app/urls.py
+++ app/urls.py
@@ -4,15 +4,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from accounts.views import login_view, logout_view, register_view
-from cars.views import CarDetailView, CarsListView, NewCarCreateView
+from cars.views import (
+    CarDeleteView,
+    CarDetailView,
+    CarsListView,
+    CarUpdateView,
+    NewCarCreateView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('cars/new/', NewCarCreateView.as_view(), name='new_car'),
     path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('cars/<int:pk>/update', CarUpdateView.as_view(), name='car_update'),
+    path('cars/<int:pk>/delete', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,12 @@
 from django.db.models.query import QuerySet
-from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 from cars.models import Car
 from cars.forms import CarModelForm
 
@@ -27,3 +34,18 @@ class NewCarCreateView(CreateView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('car_detail', kwargs={"pk": self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
