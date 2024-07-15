1360a0e93b29b5f150a16f8b4e459820c08993ef
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-15 00:45:52 -0300

    feat: openai integration

--- cars/signals.py
+++ cars/signals.py
@@ -3,13 +3,14 @@ from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(
-        total_value=Sum('value')
-    )['total_value']
+        total_value=Sum("value")
+    )["total_value"]
 
     CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value)
 
@@ -25,6 +26,13 @@ def car_post_delete(sender, instance, **kwargs) -> None:
 
 
 @receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs) -> None:
+def car_pre_save(sender, instance: Car, **kwargs) -> None:
     if not instance.bio:
-        instance.bio = 'Bio gerada automaticamente!'
+        try:
+            ai_bio = get_car_ai_bio(
+                instance.model, instance.brand, instance.model_year)
+            instance.bio = ai_bio
+        except:
+            print("Erro ao gerar bio inteligente.")
+            instance.bio = f"{instance.brand} {instance.model} {
+                instance.model_year if instance.model_year else ""}"
